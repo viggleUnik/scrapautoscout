9bbbef2fd94922d3fe70e42ad3c077110d9e7385
viggleUnik <vicol.cristianken@gmail.com>
2023-01-24 14:34:07 +0200

    refined scrapper

--- raw_code.py
+++ raw_code.py
@@ -2,8 +2,8 @@ import pandas as pd
 import requests  # for making standard html requests
 from bs4 import BeautifulSoup  # magical tool for parsing html data
 import json
-from scrapper import compose_search_url, get_all_ids_for_search_url, get_numbers_of_offers_from_url
-from typing import Dict, List, Tuple
+from scrapper import compose_search_url, get_numbers_of_offers_from_url, get_content_from_all_pages, get_article_ids, get_all_ids_for_search_url
+from typing import Dict, List, Tuple, Union
 def get_ofers_from_years():
     df_offers = pd.DataFrame(columns=['yearfrom', 'yearto'])
     for y in range(1992, 2022, 2):
@@ -33,7 +33,7 @@ def get_ofers_from_years():
 from time import sleep
 from random import randint
 
-#just commited
+
 max_results = 400
 top_makers = ['audi', 'bmw', 'ford', 'mercedes-benz', 'opel', 'volkswagen', 'renault']
 search_url = 'https://www.autoscout24.com/lst'
@@ -56,7 +56,7 @@ price_ranges = [
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
-def calculate_nr_of_pages(nr_results: int ):
+def calculate_nr_of_pages(nr_results: int) -> Tuple[int, int]:
     nr_of_pages = nr_results // 20
     last_page_articles = nr_results % 20
 
@@ -65,22 +65,21 @@ def calculate_nr_of_pages(nr_results: int ):
         return pages_last_articles
     elif last_page_articles > 0:
         nr_of_pages += 1
-    pages_last_articles = (nr_of_pages, last_page_articles)
-    return pages_last_articles
 
-def all_article_ids(search_url: str = None,
-                    max_results: int = None,
-                    makers: List[str] = None,
-                    years: List[int] = None,
-                    price_ranges: List[List[int]] = None,
-                    max_pages: int = None,
-                     ):
+    return nr_of_pages, last_page_articles
 
+
+def get_all_article_ids_forloop(
+        max_results: int = None,
+        makers: List[str] = None,
+        years: List[int] = None,
+        price_ranges: List[List[int]] = None
+):
+
+    # find results for makers
     for maker in makers:
-        search_url = compose_search_url(search_url, maker=maker)
-        n_results = get_numbers_of_offers_from_url(search_url)
-        print(f'Maker {maker} nr {n_results}')
 
+        # find results for years of registration
         for year in years:
             search_url = 'https://www.autoscout24.com/lst'
             search_url = compose_search_url(search_url, maker=maker, fregfrom=year, fregto=year)
@@ -88,36 +87,49 @@ def all_article_ids(search_url: str = None,
             n_results = get_numbers_of_offers_from_url(search_url)
 
             if n_results == 0:
-                pass
+                continue
             elif n_results < max_results:
-                pages_articles = calculate_nr_of_pages(n_results)
-                print(f'year {year} res {n_results} nr_pages {pages_articles[0]} article on last {pages_articles[1]}')
+                nr_of_pages, last_page_articles = calculate_nr_of_pages(nr_results=n_results)  # unpacking
+                ids = get_all_ids_for_search_url(search_url, nr_of_pages, last_page_articles)
 
             elif n_results > max_results:
-                print(f'year {year} total res {n_results}')
                 for price_from, price_to in price_ranges:
                     search_url = 'https://www.autoscout24.com/lst'
                     search_url = compose_search_url(search_url, maker=maker, fregfrom=year, fregto=year,
                                                     pricefrom=price_from, priceto=price_to)
                     n_results = get_numbers_of_offers_from_url(search_url)
                     if n_results == 0:
-                        pass
+                        continue
                     elif n_results > 0:
-                        pages_articles = calculate_nr_of_pages(n_results)
-                        print(f'res {n_results} nr_pages {pages_articles[0]} article on last {pages_articles[1]} pricefrom {price_from} priceto {price_to}')
+                        nr_of_pages, last_page_articles = calculate_nr_of_pages(nr_results=n_results)
+                        ids = get_all_ids_for_search_url(search_url, nr_of_pages, last_page_articles)
 
 
-all_article_ids(search_url='https://www.autoscout24.com/lst',
-                max_results=400,
-                makers=['audi'],
-                years=list(range(1992,2023)),
-                price_ranges=price_ranges
-                )
+get_all_article_ids_forloop(search_url='https://www.autoscout24.com/lst',
+                            max_results=400,
+                            makers=['renault'],
+                            years=list(range(1992, 2023)),
+                            price_ranges=price_ranges
+                            )
 
 
-# scoatem numar flexibil de articole cand avem < 400
-# n_offers /20 art per pagina, aflam cate pagini sunt si scoatem
-# n_ofers mod 20 -> numarul de articole de ultima pagina incompleta
+# nr_offers = get_numbers_of_offers_from_url(
+#     'https://www.autoscout24.com/lst/bmw?fregfrom=1992&fregto=1992')
+# print(f'nr offers {nr_offers}')
+# nr_pages_articles = calculate_nr_of_pages(nr_offers)
+# print(f'pages articles {nr_pages_articles}')
+#
+# article_ids = get_all_ids_for_search_url(
+#     'https://www.autoscout24.com/lst/bmw?fregfrom=1992&fregto=1992',
+#     nr_pages_articles[0], nr_pages_articles[1])
+# print(article_ids)
+# print(f'lenght {len(article_ids)}')
 
-
+# with open(
+#     'cache/aHR0cHM6Ly93d3cuYXV0b3Njb3V0MjQuY29tL2xzdC9hdWRpP2ZyZWdmcm9tPTIwMDEmZnJlZ3RvPTIwMDEmcHJpY2Vmcm9tPTEzMDAxJnByaWNldG89MTYwMDA.json',
+#     'r') as file:
+#     json_object = json.load(file)
+#
+# for id in json_object:
+#     print(id)
 
--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,13 @@
+import os.path
+
 import requests  # for making standard html requests
 from bs4 import BeautifulSoup  # magical tool for parsing html data
 import json  # for parsing data
 from time import sleep
 from random import randint
-from typing import Dict, List
+from typing import Dict, List, Tuple
 import logging
+import base64
 
 
 log = logging.getLogger(__name__)
@@ -101,25 +104,63 @@ def get_content_from_all_pages(
     return pages
 
 
-def get_article_ids(pages: List[BeautifulSoup]) -> List[str]:
+def get_article_ids(pages: List[BeautifulSoup], nr_articles_last: int = None) -> List[str]:
     """
     Get all articles IDs in a list of pages
     :param pages: list of BS
     :return: list of article IDs
     """
+    # case when we have full pages, 20 articles per page
     article_ids = []
-    for page in pages:
-        # Find all articles id on a page
-        articles_per_page = page.find_all('article')
-        for article in articles_per_page:
+    if nr_articles_last == 0:
+        for page in pages:
+            # Find all articles id on a page
+            articles_per_page = page.find_all('article')
+            for article in articles_per_page:
+                article_ids.append(article.get('id'))
+
+    # case when we have 1 incomplete page
+    elif len(pages) == 1:
+        last_page_articles = pages[0].find_all('article')
+        for count, article in enumerate(last_page_articles):
+            if count >= nr_articles_last:
+                break
+            article_ids.append(article.get('id'))
+
+    # case when we have pages with one last page incomplete
+    elif len(pages) > 1:
+        for page in pages[:-1]:
+            articles_per_page = page.find_all('article')
+            for article in articles_per_page:
+                article_ids.append(article.get('id'))
+
+        last_page_articles = pages[-1].find_all('article')
+
+        for count, article in enumerate(last_page_articles):
+            if count >= nr_articles_last:
+                break
             article_ids.append(article.get('id'))
 
     return article_ids
 
 
-def get_all_ids_for_search_url(search_url: str, max_pages: int = MAX_PAGES):
-    list_bs = get_content_from_all_pages(search_url, max_pages=max_pages)
-    ids = get_article_ids(list_bs)
+def get_all_ids_for_search_url(search_url: str, max_pages: int, last_page_articles: int):
+    # name_file = base64.b64encode(search_url.encode()).decode().replace('=', '') + '.json'
+    name_file = base64.b64encode(search_url.encode()).decode()
+    name_file = ''.join(c for c in name_file if c.isalnum())
+    name_file = name_file + '.json'
+
+    path_file = f'cache/{name_file}'
+
+    if os.path.exists(path_file):
+        with open(path_file) as f:
+            ids = json.load(f)
+    else:
+        list_bs = get_content_from_all_pages(search_url, max_pages=max_pages)
+        ids = get_article_ids(list_bs, last_page_articles)
+        with open(path_file, 'w') as f:
+            json.dump(ids, f)
+
     return ids
 
 
@@ -182,73 +223,51 @@ def get_numbers_of_offers_from_url(url: str) -> int:
     return int(n_offers)
 
 
-def get_json_data_from_articles(article_ids: List[str]):
-    return None
+def calculate_nr_of_pages(nr_results: int) -> Tuple[int, int]:
+    nr_of_pages = nr_results // 20
+    last_page_articles = nr_results % 20
+
+    if nr_of_pages > 20:
+        pages_last_articles = (20, 0)
+        return pages_last_articles
+    elif last_page_articles > 0:
+        nr_of_pages += 1
+
+    return nr_of_pages, last_page_articles
 
 
-def has_maker(search_url: str) -> bool:
-    return '/lst/' in search_url
-
-
-def has_freg(search_url: str) -> bool:
-    return 'fregfrom=' in search_url or 'fregto=' in search_url
-
-
-def has_price(search_url: str) -> bool:
-    return 'pricefrom=' in search_url or 'priceto=' in search_url
-
-
-def get_all_article_ids(
-        search_url: str = None,
-        max_results: int = MAX_RESULTS,
+def get_all_article_ids_forloop(
+        max_results: int = None,
         makers: List[str] = None,
         years: List[int] = None,
-        price_ranges: List[List[int]] = None,
-        max_pages: int = MAX_PAGES,
+        price_ranges: List[List[int]] = None
 ):
-    if makers is None:
-        makers = MAKERS
 
-    if years is None:
-        years = YEARS
+    # find results for makers
+    for maker in makers:
 
-    if price_ranges is None:
-        price_ranges = PRICE_RANGES
+        # find results for years of registration
+        for year in years:
+            search_url = 'https://www.autoscout24.com/lst'
+            search_url = compose_search_url(search_url, maker=maker, fregfrom=year, fregto=year)
+            print(search_url)
+            n_results = get_numbers_of_offers_from_url(search_url)
 
-    if search_url is None:
-        search_url = f'{SITE_URL}/lst'
+            if n_results == 0:
+                continue
+            elif n_results < max_results:
+                nr_of_pages, last_page_articles = calculate_nr_of_pages(nr_results=n_results)  # unpacking
+                ids = get_all_ids_for_search_url(search_url, nr_of_pages, last_page_articles)
 
-    n_results = get_numbers_of_offers_from_url(search_url)
-    # log.debug(f'{search_url} has {n_results} results')
-    print(f'{search_url} has {n_results} results')
+            elif n_results > max_results:
+                for price_from, price_to in price_ranges:
+                    search_url = 'https://www.autoscout24.com/lst'
+                    search_url = compose_search_url(search_url, maker=maker, fregfrom=year, fregto=year,
+                                                    pricefrom=price_from, priceto=price_to)
+                    n_results = get_numbers_of_offers_from_url(search_url)
+                    if n_results == 0:
+                        continue
+                    elif n_results > 0:
+                        nr_of_pages, last_page_articles = calculate_nr_of_pages(nr_results=n_results)
+                        ids = get_all_ids_for_search_url(search_url, nr_of_pages, last_page_articles)
 
-    if n_results == 0:
-        print(f'no results found')
-        return []
-    elif n_results <= max_results:
-        print(f'{n_results} less than {max_results}: get all IDs')
-        ids = get_all_ids_for_search_url(search_url, max_pages=max_pages)
-        return ids
-    else:
-        if not has_maker(search_url):
-            print(f'refine the search - add maker')
-            for maker in makers:
-                search_url = compose_search_url(search_url, maker=maker)
-                ids = get_all_article_ids(search_url)
-                return ids
-        elif not has_freg(search_url):
-            print(f'refine the search - add freg year')
-            for year in years:
-                search_url = compose_search_url(search_url, fregfrom=year, fregto=year)
-                ids = get_all_article_ids(search_url)
-                return ids
-        elif not has_price(search_url):
-            print(f'refine the search - add price range')
-            for pricefrom, priceto in price_ranges:
-                search_url = compose_search_url(search_url, pricefrom=pricefrom, priceto=priceto)
-                ids = get_all_article_ids(search_url)
-                return ids
-        else:
-            print(f'no further query refining possible: get {max_pages*20} IDs out of {n_results}')
-            ids = get_all_ids_for_search_url(search_url, max_pages=max_pages)
-            return ids

1446e9d98f131fd1ac5c2cb2bd2d4d9a14284c8b
viggleUnik <vicol.cristianken@gmail.com>
2023-02-02 17:21:45 +0200

    delete junk

--- linkList.py
+++
@@ -1,8 +0,0 @@
-
-sortByPriceWithMake = 'https://www.autoscout24.com/lst/audi?sort=standard&pricefrom=3000&priceto=5000'
-sortByPrice = 'https://www.autoscout24.com/lst?sort=standard&pricefrom=3000&priceto=5000'
-
-make = [ 'audi', 'bmw', 'ford', 'mercedes-benz', 'opel', 'volkswagen', 'renault']
-sortByMake = 'https://www.autoscout24.com/lst/bmw'
-
-

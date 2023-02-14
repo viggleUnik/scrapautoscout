0b20446f1e603b0f16366da93b7c6aeb414594d3
viggleUnik <vicol.cristianken@gmail.com>
2023-02-14 17:33:59 +0200

    run modification

--- scrapautoscout/cli.py
+++ scrapautoscout/cli.py
@@ -7,18 +7,16 @@ from scrapautoscout.scrapper import get_all_article_ids_forloop, read_ids_json_f
 
 
 def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir_out', default=config.DIR_CACHE,
+                        help='Where to save artifacts. Default: /cache from project root')
+    args = parser.parse_args()
+    # override default folder for cache
+    config.DIR_CACHE = args.dir_out
+    os.makedirs(config.DIR_CACHE, exist_ok=True)
     get_all_article_ids_forloop()
     read_ids_json_files_from_cache()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dir_out', default=config.DIR_CACHE,
-                        help='Where to save artifacts. Default: /cache from project root')
-    args = parser.parse_args()
-
-    # override default folder for cache
-    config.DIR_CACHE = args.dir_out
-    os.makedirs(config.DIR_CACHE, exist_ok=True)
-
     run()

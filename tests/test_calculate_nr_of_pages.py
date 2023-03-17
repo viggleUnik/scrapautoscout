1953f4ef4e3e7300c6472aeb6aedadc0cac92b4b
nicolaiv <nicolaiv@wix.com>
2023-03-17 12:18:43 +0200

    add test for calculate_nr_of_pages()

---
+++ tests/test_calculate_nr_of_pages.py
@@ -0,0 +1,19 @@
+import unittest
+
+from scrapautoscout.scrapper import calculate_nr_of_pages
+
+
+class TestGetAllArticleIDs(unittest.TestCase):
+
+    def test_various_inputs(self):
+        self.assertEqual((1, 1), calculate_nr_of_pages(1))
+        self.assertEqual((1, 10), calculate_nr_of_pages(10))
+        self.assertEqual((2, 1), calculate_nr_of_pages(21))
+        self.assertEqual((5, 0), calculate_nr_of_pages(100))
+        self.assertEqual((6, 1), calculate_nr_of_pages(101))
+        self.assertEqual((20, 1), calculate_nr_of_pages(381))
+        self.assertEqual((20, 0), calculate_nr_of_pages(400))
+        self.assertEqual((20, 0), calculate_nr_of_pages(401))
+        self.assertEqual((20, 0), calculate_nr_of_pages(421))
+        self.assertEqual((20, 0), calculate_nr_of_pages(495))
+        self.assertEqual((20, 0), calculate_nr_of_pages(99999))

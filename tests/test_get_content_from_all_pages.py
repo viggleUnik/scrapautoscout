f10c0043b830ca4f79b12b548c82fa632c6f4c33
nicolaiv <nicolaiv@wix.com>
2023-03-19 01:19:56 +0200

    *

--- tests/test_get_content_from_all_pages.py
+++ tests/test_get_content_from_all_pages.py
@@ -7,6 +7,9 @@ from scrapautoscout.scrapper import get_content_from_all_pages
 class test_get_content_from_all_pages(unittest.TestCase):
 
     def test_default(self):
-        pages = get_content_from_all_pages('https://www.autoscout24.com/lst?', max_pages=2)
+        pages = get_content_from_all_pages('https://www.autoscout24.com/lst?', max_pages=20)
         self.assertTrue(len(pages) > 0)
         self.assertTrue(isinstance(pages[0], BeautifulSoup))
+        # execution time 106.433s
+        # - requests.get: 67%
+        # - get_valid_proxies_multithreading(): 31%

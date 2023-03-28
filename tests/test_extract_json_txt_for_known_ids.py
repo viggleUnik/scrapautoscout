fc524df27660aea4fad6d4a662654e43e7f104ce
viggleUnik <vicol.cristianken@gmail.com>
2023-03-28 14:09:35 +0300

    test name , location changes

--- tests/test_extract_json_txt_for_known_ids.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from scrapautoscout.scrapper import extract_json_txt_for_known_ids
-
-
-class test_extract_json_txt_for_known_ids(unittest.TestCase):
-
-    def test_local(self):
-        extract_json_txt_for_known_ids(location='local', chunk_size=100)

b88e620b50860eb537f35795c0d55423ca270ec1
viggleUnik <vicol.cristianken@gmail.com>
2023-03-28 14:09:31 +0300

    test name , location changes

--- tests/test_get_all_ids_for_search_url.py
+++ tests/test_get_all_ids_for_search_url.py
@@ -7,6 +7,6 @@ class test_get_all_ids_for_search_url(unittest.TestCase):
 
     def test_default(self):
         url = compose_search_url(maker='BMW', fregfrom=1981, fregto=1982, priceto=10_000)
-        ids = get_all_ids_for_search_url(url)
+        ids = get_all_ids_for_search_url(search_url=url, cache_location='s3')
         self.assertTrue(len(ids) > 0)
         print('\n'.join(ids))
---
+++ tests/test_main_extract_json_txt_for_all_known_ids.py
@@ -0,0 +1,9 @@
+import unittest
+
+from scrapautoscout.scrapper import main_extract_json_txt_for_all_known_ids
+
+
+class test_main_extract_json_txt_for_all_known_ids(unittest.TestCase):
+
+    def test_local(self):
+        main_extract_json_txt_for_all_known_ids(location='local', chunk_size=100)
